73b9979bd2d6f034d88b6d190bda3eebee80af45
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-02-21 00:46:30 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,21 +1,21 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def create_and_read_file(filename):
+def append_hello_world(filename)
     # Write your code here
     pass
 
 # Exercise 2
-def write_user_info(filename, name, age):
+def is_file_nonempty(filename)
     # Write your code here
     pass
 
 # Exercise 3
-def count_words_in_file(filename):
+def count_lines(filename)
     # Write your code here
     pass
 
 # Exercise 4
-def find_longest_word_in_file(filename):   
+def count_characters_excluding_spaces(filename)
     # Write your code here
     pass
