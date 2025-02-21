0d18b0998548d43fff3c106573176c014a4371ca
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-02-21 09:57:53 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,21 +1,21 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def append_hello_world(filename)
+def append_hello_world(filename):
     # Write your code here
     pass
 
 # Exercise 2
-def is_file_nonempty(filename)
+def is_file_nonempty(filename):
     # Write your code here
     pass
 
 # Exercise 3
-def count_lines(filename)
+def count_lines(filename):
     # Write your code here
     pass
 
 # Exercise 4
-def count_characters_excluding_spaces(filename)
+def count_characters_excluding_spaces(filename):
     # Write your code here
     pass
